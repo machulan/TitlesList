8f7e74b338db95fd5089100d78d761df341354a0
Nik <nik.machula@yandex.ru>
2018-07-04 15:54:26 +0400

    Refactor.

--- src/components/TitlesList/index.js
+++ src/components/TitlesList/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
-import TitlesListItem from '../TitlesListItem/index';
+import TitlesListItem from '../TitlesListItem';
 
 class TitlesList extends React.Component {
     render() {
