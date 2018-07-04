96cbf1a27bee79da97be25475e8e287c6e0c28ff
Nik <nik.machula@yandex.ru>
2018-07-04 17:05:43 +0400

    Refactor.

--- src/components/TitlesList/index.js
+++ src/components/TitlesList/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import styles from './styles.module.css';
-import TitlesListItem from '../TitlesListItem/index';
+import TitlesListItem from '../TitlesListItem';
 import {observer, inject} from 'mobx-react';
 
 @inject('titlesListStore')
 @observer
 class TitlesList extends React.Component {
     render() {
-        const titlesListStore = this.props.titlesListStore;
+        const { titlesListStore } = this.props;
 
         return (
             <section className={styles.container}>
                 <div className={styles.content}>
                     {
-                        titlesListStore.titlesList.map(titleListItemStore => {
-                            return <TitlesListItem key={titleListItemStore.key} title={titleListItemStore.title} publicationPlace={titleListItemStore.publicationPlace} />
-                        })
+                        titlesListStore.titles.map(titlesListItemStore => (
+                            <TitlesListItem key={titlesListItemStore.key} title={titlesListItemStore.title} publicationPlace={titlesListItemStore.publicationPlace} />
+                        ))
                     }
                 </div>
             </section>
--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,20 @@
 import {observable, action} from 'mobx';
+import TitleListItemStore from './titleListItemStore';
 import 'babel-polyfill';
 
-class TitleListItemStore {
-    key;
-    @observable title;
-    @observable publicationPlace;
-
-    constructor(key, title, publicationPlace) {
-        this.key = key;
-        this.title = title;
-        this.publicationPlace = publicationPlace;
-    }
-}
-
 class TitlesListStore {
-    @observable titlesList = [];
+    @observable titles = [];
     @observable searchInputValue = '';
 
     @action setSearchInputValue = (value) => {
         this.searchInputValue = value;
     }
 
-    formatTitle = (title) => {
-        return title.substr(0, 23);
-    }
-
-    formatPublicationPlace = (publicationPlace) => {
-        return publicationPlace.substr(0, 45);
-    }
-
     @action async fetchData() {
-        const url = 'https://chroniclingamerica.loc.gov/search/titles/results/?terms=' + this.searchInputValue + '&format=json&page=1';
+        const url = `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${this.searchInputValue}&format=json&page=1`;
         let response = await fetch(url);
         let data = await response.json();
-        this.titlesList = data.items.map(item => new TitleListItemStore(item.id, this.formatTitle(item.title), this.formatPublicationPlace(item.place_of_publication)));
+        this.titles = data.items.map(item => new TitleListItemStore(item.id, item.title, item.place_of_publication));
     }
 }
 
